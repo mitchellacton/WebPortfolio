b8db4386a7621dc847bb8e0de113d00cfc86859b
mitchellacton <66482029+mitchellacton@users.noreply.github.com>
2020-08-06 20:42:33 -0400

    fixed an issue causing dark mode switch to be in wrong position on initial site load

--- site/darkMode.js
+++ site/darkMode.js
@@ -2,24 +2,28 @@ document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
 if (sessionStorage.getItem('bg') === '#eee') {
     document.getElementById("darkSwitch").checked = false;
-}else{ document.getElementById("darkSwitch").checked = true;}
+    document.body.style.backgroundColor = sessionStorage.getItem('bg');
+
+}else{ document.getElementById("darkSwitch").checked = true;
+       document.body.style.backgroundColor = sessionStorage.getItem('bg');
+}
 function darkMode() {
      if ( sessionStorage.getItem('bg') === '#eee') {
 
             sessionStorage.setItem('bg', 'rgb(34,34,34)');
-            sessionStorage.setItem('cc', '#777');
+            //sessionStorage.setItem('cc', '#777'); change this for future font colour adjustment
             
 
      }
     else if (sessionStorage.getItem('bg') == null || undefined) {
         sessionStorage.setItem('bg', 'rgb(34,34,34)');
-        sessionStorage.setItem('cc', '#777');
+        //sessionStorage.setItem('cc', '#777');
 
     }
     else if( sessionStorage.getItem('bg') === 'rgb(34,34,34)') {
 
         sessionStorage.setItem('bg', '#eee');
-        sessionStorage.setItem('cc', '#333');
+        //sessionStorage.setItem('cc', '#333');
 
 
     }
